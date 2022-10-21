907c23f20276aeb7aa3f97e92870c3ecc2d68718
manan jain <60469574+mananjain31@users.noreply.github.com>
2022-10-22 01:26:51 +0530

    Create binary-to-decimal.py

---
+++ binary-to-decimal.py
@@ -0,0 +1,16 @@
+def binaryToDecimal(binary):
+	
+	binary1 = binary
+	decimal, i, n = 0, 0, 0
+	while(binary != 0):
+		dec = binary % 10
+		decimal = decimal + dec * pow(2, i)
+		binary = binary//10
+		i += 1
+	print(decimal)
+	
+
+binaryToDecimal(1001)
+binaryToDecimal(11)
+binaryToDecimal(10)
+binaryToDecimal(101)
